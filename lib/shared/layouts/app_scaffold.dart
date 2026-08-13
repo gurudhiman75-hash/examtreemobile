73f32bd90cb37d5fe848f1d2c8d20b8ab71f1b64
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-08-13 20:47:21 +0530

    feat(companion): expose persistent daily action

--- lib/shared/layouts/app_scaffold.dart
+++ lib/shared/layouts/app_scaffold.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../features/companion/presentation/daily_companion_screen.dart';
+
 class AppScaffold extends StatelessWidget {
   final StatefulNavigationShell navigationShell;
 
@@ -16,10 +18,26 @@ class AppScaffold extends StatelessWidget {
     );
   }
 
+  void _openDailyCompanion(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) => const DailyCompanionScreen(),
+        fullscreenDialog: true,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: navigationShell,
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => _openDailyCompanion(context),
+        tooltip: 'Open Daily Companion',
+        icon: const Icon(Icons.auto_awesome_rounded),
+        label: const Text('Daily'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       bottomNavigationBar: NavigationBar(
         selectedIndex: navigationShell.currentIndex,
         onDestinationSelected: _goBranch,
