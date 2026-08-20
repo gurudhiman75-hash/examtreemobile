e596bb2ca6988cb76646c96c60e64f278ff1891d
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-08-20 11:32:55 +0530

    Keep Home provider surface minimal

--- lib/features/home/presentation/providers/home_preparation_providers.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../../../companion/presentation/providers/daily_companion_providers.dart';
-
-final homeDueRevisionCountProvider = Provider<AsyncValue<int>>((ref) {
-  final companionAsync = ref.watch(dailyCompanionSnapshotProvider);
-  final now = ref.watch(dailyCompanionClockProvider)();
-  return companionAsync.whenData((snapshot) => snapshot.dueItems(now).length);
-});
