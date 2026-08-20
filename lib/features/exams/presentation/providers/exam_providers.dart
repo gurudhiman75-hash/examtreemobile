d87cb725ef06d1bb894265ebc311ef16a0a49a88
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-08-20 10:51:45 +0530

    Localize canonical exam questions from device preference

--- lib/features/exams/presentation/providers/exam_providers.dart
+++ lib/features/exams/presentation/providers/exam_providers.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../core/models/exam_model.dart';
 import '../../../../core/models/question_model.dart';
 import '../../../../core/providers/repository_providers.dart';
+import '../../../preferences/domain/question_language.dart';
+import '../../../preferences/presentation/providers/question_language_providers.dart';
 
 final availableExamsProvider = FutureProvider<List<Exam>>((ref) async {
   final repository = ref.watch(examRepositoryProvider);
@@ -21,5 +23,9 @@ final examDetailsProvider = FutureProvider.family<Exam, String>((ref, examId) as
 
 final examQuestionsProvider = FutureProvider.family<List<Question>, String>((ref, examId) async {
   final repository = ref.watch(examRepositoryProvider);
-  return repository.getExamQuestions(examId);
+  final language = await ref.watch(questionLanguageProvider.future);
+  final questions = await repository.getExamQuestions(examId);
+  return questions
+      .map((question) => localizeQuestion(question, language))
+      .toList(growable: false);
 });
