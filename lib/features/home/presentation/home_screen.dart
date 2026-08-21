8d6a76c1a113e7c0ef20941917562605134bc24c
gurudhiman75-hash <gurudhiman75@gmail.com>
2026-08-21 20:45:26 +0530

    Make Home V8 the active home experience

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,3 +1,4 @@
 export 'home_screen_v5.dart' hide HomeScreen;
 export 'home_screen_v6.dart' hide HomeScreen;
-export 'home_screen_v7.dart';
+export 'home_screen_v7.dart' hide HomeScreen;
+export 'home_screen_v8.dart';
